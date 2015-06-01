776a7beae0f068a8cf08900265c5aaebb33946a4
Rob van Aarle <robvanaarle@gmail.com>
2015-06-01 06:11:57 +0200

    refactored mysql pattern matcher

--- src/ultimo/waf/mysql/MySqlTokenMatcher.php
+++ src/ultimo/waf/mysql/MySqlTokenMatcher.php
@@ -25,24 +25,8 @@ class MySqlTokenMatcher {
     }
     $subject = ' ' . implode(' ', $tokenTypes);
     
-    // convert token pattern to regex pattern
-    $elems = explode(' ', $tokenPattern);
-    
-    $pattern = array();
-    foreach ($elems as $elem) {
-      $elem = trim($elem);
-      if (strlen($elem) == 0) {
-        continue;
-      }
-      
-      $elem = preg_replace('/\%[a-z0-9\.\_]+/', '( \\\\$0)', $elem);
-      
-      $pattern[] = $elem;
-    }
-    
-    $pattern = implode('', $pattern);
-
-    //echo "\n'{$pattern}'\n'{$subject}'\n";
+    // convert token pattern to regex pattern: remove whitespace and a single space before each token type
+    $pattern = preg_replace('/\%[a-z0-9\.\_]+/', '( \\\\$0)', str_replace(' ', '', $tokenPattern));
     
     return preg_match($pattern, $subject) == 1;
   }
